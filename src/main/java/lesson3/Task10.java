b9511e75d3680c4985e92e9130aea9f6bb11e632
bedon <dev.bedon@gmail.com>
2019-05-28 23:23:42 +0300

    add task10

---
+++ src/main/java/lesson3/Task10.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+        System.out.println("***********");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("*         *");
+        }
+        System.out.println("***********");
+    }
+}
