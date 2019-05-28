474d4b6990c368c25d8476f3e66570988300076b
bedon <dev.bedon@gmail.com>
2019-05-28 22:23:49 +0300

    111

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -3,7 +3,7 @@ package lesson1;
 public class Task1 {
     public static void main(String[] args) {
 
-        String s = "some string111";
+        String s = "some string";
 
         for (int i = 0; i < 5; i++) {
             System.out.println(s);
