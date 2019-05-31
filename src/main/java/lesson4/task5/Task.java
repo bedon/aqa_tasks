fa575668873f017ab4697c8d10621741de2752e6
bedon <dev.bedon@gmail.com>
2019-06-01 00:39:15 +0300

    lesson4 task5 done

---
+++ src/main/java/lesson4/task5/Task.java
@@ -0,0 +1,35 @@
+package lesson4.task5;
+
+public class Task {
+
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        public String name;
+        public int age;
+        public int weight;
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+}
