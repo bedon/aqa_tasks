4f1626e67c43a60a0bf233a7497024df16d371b4
bedon <dev.bedon@gmail.com>
2019-06-01 00:33:46 +0300

    lesson4 task4 done

---
+++ src/main/java/lesson4/task4/Task.java
@@ -0,0 +1,45 @@
+package lesson4.task4;
+
+
+import com.sun.corba.se.spi.orbutil.threadpool.Work;
+
+public class Task {
+    public static void main(String[] args) {
+    }
+
+    public class Worker {
+
+    }
+
+    public class Clerk extends Worker {
+
+    }
+
+    public class IT extends Worker {
+
+    }
+
+    public class Programmer extends IT{
+
+    }
+
+    public class ProjectManager extends IT {
+
+    }
+
+    public class CTO extends IT {
+
+    }
+
+    public class OfficeManager extends Clerk {
+
+    }
+
+    public class HR extends Clerk {
+
+    }
+
+    public class Cleaner extends Clerk {
+
+    }
+}
