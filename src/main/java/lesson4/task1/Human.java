f7bd0624fd92f33356e4998e96e5b18b031beeef
bedon <dev.bedon@gmail.com>
2019-06-01 00:01:21 +0300

    lesson4 task1 added

---
+++ src/main/java/lesson4/task1/Human.java
@@ -0,0 +1,17 @@
+package lesson4.task1;
+
+public class Human {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int height;
+    private int weight;
+
+    public Human(String firstName, String lastName, int age, int height, int weight) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/lesson4/task2/Boxer.java
@@ -0,0 +1,4 @@
+package lesson4.task2;
+
+public class Boxer {
+}
