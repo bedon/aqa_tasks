8102451bee15e6316a5bbbef19f0dc008fdea7b7
bedon <dev.bedon@gmail.com>
2019-06-03 20:02:23 +0300

    lesson5 task5 done

---
+++ src/main/java/lesson5/Task5.java
@@ -0,0 +1,18 @@
+package lesson5;
+
+import java.util.ArrayList;
+
+public class Task5 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add("asd1");
+        list.add("asd2");
+        list.add("asd3");
+        list.add("asd4");
+        list.add("asd5");
+
+        for (String s : list)
+            System.out.println(s);
+    }
+}
