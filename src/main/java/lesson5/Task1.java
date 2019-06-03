68f96da8156af0c57b865a987c56e4a635f9e6ba
bedon <dev.bedon@gmail.com>
2019-06-03 19:59:47 +0300

    lesson5 task1 some changes

--- src/main/java/lesson5/Task1.java
+++ src/main/java/lesson5/Task1.java
@@ -6,13 +6,6 @@ public class Task1 {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
 
-        list.add("asd1");
-        list.add("asd2");
-        list.add("asd3");
-        list.add("asd4");
-        list.add("asd5");
-
-        for (String s : list)
-            System.out.println(s);
+        System.out.println(list);
     }
 }
