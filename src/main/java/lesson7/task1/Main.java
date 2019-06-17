9ed91817cf8804a482f06340ade7c743ef50428e
bedon <dev.bedon@gmail.com>
2019-06-17 22:14:51 +0300

    lesson7 task1 done

--- src/main/java/lesson7/task1/Main.java
+++ src/main/java/lesson7/task1/Main.java
@@ -4,10 +4,10 @@ import java.lang.reflect.Method;
 
 public class Main {
     /**
-    1. Создать аннотацию, которая принимает параметры для метода.
-    Написать код, который вызовет метод, помеченный этой аннотацией,
-    и передаст параметры аннотации в вызываемый метод.
-    */
+     * 1. Создать аннотацию, которая принимает параметры для метода.
+     * Написать код, который вызовет метод, помеченный этой аннотацией,
+     * и передаст параметры аннотации в вызываемый метод.
+     */
     @TestAnnotation(a = 8, b = 13)
     public void test(int a, int b) {
         System.out.println("a = " + a);
@@ -17,15 +17,18 @@ public class Main {
     public static void main(String[] args) {
         Main m = new Main();
 
-        try {
-            Class<?> mClass = m.getClass();
-            Method method = mClass.getMethod("test", int.class, int.class);
-            TestAnnotation testAnnotation = method.getAnnotation(TestAnnotation.class);
+        Class<?> mClass = m.getClass();
+        Method[] methods = mClass.getDeclaredMethods();
 
-            m.test(testAnnotation.a(), testAnnotation.b());
-        } catch (NoSuchMethodException e) {
-            System.out.println("No such method");
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(TestAnnotation.class)) {
+                TestAnnotation testAnnotation = method.getAnnotation(TestAnnotation.class);
+                try {
+                    method.invoke(m, testAnnotation.a(), testAnnotation.b());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
     }
 }
